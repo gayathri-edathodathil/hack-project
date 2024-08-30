35845b46c7256a6ab1e46684c8114940eaae0566
gayathri-edathodathil <gayathriedathodathil@gmail.com>
2024-08-31 04:18:58 +0530

    initial-step

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+  <>
+
+    <h1 className='text-4xl bg-slate-600'>Hello word</h1>
+
+  </>
+  )
+}
+
+export default App
